b5093337321eae07d902db6fa5be711e5ae10767
x23d8 <lenguyenquocanh1999@gmail.com>
2026-03-20 00:08:02 +0700

    change method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/landing");
-  }, [router]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="animate-spin w-8 h-8 border-4 border-teal-500 border-t-transparent rounded-full" />
-    </div>
-  );
+  redirect("/landing");
 }
